ed344b968eb8cfcbc80d8b12afaf54a05d44e6e7
Rodrigo Patiño <developer.rodrigo.patino@gmail.com>
2023-08-08 23:57:59 -0600

    Timeout

--- lib/sender.ex
+++ lib/sender.ex
@@ -1,17 +1,14 @@
 defmodule Sender do
   def send_email(email) do
-    Process.sleep(3 * 1000)
+    Process.sleep(7 * 1000)
     IO.puts("Email to #{email} sent")
     {:ok, "email_sent"}
   end
 
+
   def notify_all(emails) do
     emails
-    |> Enum.map(fn email ->
-      Task.async(fn ->
-        send_email(email)
-      end)
-    end)
-    |> Enum.map(&Task.await/1)
+    |> Task.async_stream(&send_email/1, max_concurrency: 2, on_timeout: :kill_task)
+    |> Enum.to_list()
   end
 end
