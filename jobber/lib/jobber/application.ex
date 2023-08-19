51b157d84e9519f3b335e047895192a5c31907ca
Rodrigo Patiño <developer.rodrigo.patino@gmail.com>
2023-08-19 17:56:40 -0600

    Registry

--- jobber/lib/jobber/application.ex
+++ jobber/lib/jobber/application.ex
@@ -2,12 +2,10 @@ defmodule Jobber.Application do
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
   @moduledoc false
-
   use Application
 
   @impl true
   def start(_type, _args) do
-
     job_runner_config = [
       strategy: :one_for_one,
       max_seconds: 30,
@@ -15,6 +13,7 @@ defmodule Jobber.Application do
     ]
 
     children = [
+      {Registry, keys: :unique, name: Jobber.JobRegistry},
       {DynamicSupervisor, job_runner_config}
     ]
 
